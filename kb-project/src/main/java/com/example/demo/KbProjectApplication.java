6b981c7071935e00605da56d148248b48362e8e4
junwon9824 <junwon1131@naver.com>
2023-05-06 14:36:54 +0900

    kb

---
+++ kb-project/src/main/java/com/example/demo/KbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbProjectApplication.class, args);
+	}
+
+}
---
+++ kb-project/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+	private String password;
+
+}
