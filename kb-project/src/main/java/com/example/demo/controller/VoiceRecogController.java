1563848fb5244b1b22ca25a1503dfba35346752e
Keon Kim <fnc6488@gmail.com>
2023-05-29 16:36:34 +0900

    separate voice recognition page

--- kb-project/src/main/java/com/example/demo/controller/VoiceRecogController.java
+++ kb-project/src/main/java/com/example/demo/controller/VoiceRecogController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.BankAccount;
 import com.example.demo.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class VoiceRecogController {
@@ -19,7 +17,7 @@ public class VoiceRecogController {
     public String voiceRecognition(Model model, HttpServletRequest request) {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        return "voiceRecog";
+        return "voiceRecognition/voiceRecog";
 
     }
 }
